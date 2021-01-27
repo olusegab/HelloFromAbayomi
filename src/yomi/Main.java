88ffd208ffec5faf12c68a7d9d1c391fd51a8eb9
olusegab <oabayomi24@gmail.com>
2021-01-27 23:07:23 +0100

    Initial commit

---
+++ src/yomi/Main.java
@@ -0,0 +1,11 @@
+package yomi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, my name is Abayomi");
+
+
+    }
+}
