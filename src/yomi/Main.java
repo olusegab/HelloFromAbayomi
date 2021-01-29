0e323263c050782f274eee42dfe325aaf1eaf6a6
VictorB <victorb62@byui.edu>
2021-01-29 17:09:01 -0600

    Victor's changes added.

--- src/yomi/Main.java
+++ src/yomi/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Abayomi");
-
-
+        System.out.println("Hello Abayomi, my name is Victor Boxall");
     }
 }
